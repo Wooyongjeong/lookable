8c2e55afc125040d1dda7f610ef78702589d6bd3
wooyongjeong <wooyong.dev@gmail.com>
2023-04-06 01:41:18 +0900

    :ambulance: Feat: "회원가입 시 name 파라미터 필수값 아닌 것으로 변경"
    현재 회원가입 화면에 이름 설정이 없어 이를 수정

--- src/main/java/com/lookable/dto/auth/request/RegisterRequest.java
+++ src/main/java/com/lookable/dto/auth/request/RegisterRequest.java
@@ -15,7 +15,6 @@ public class RegisterRequest {
     @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 
-    @NotBlank(message = "이름을 입력해주세요.")
     private String name;
 
     @NotBlank(message = "별명을 입력해주세요.")
