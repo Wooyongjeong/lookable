9927830e1a75e00b0160f3c2c6cd1ca41ca72fd8
정우용 <wooyong.dev@gmail.com>
2023-05-10 23:14:54 +0900

    :truck: Style: "Rename ChangeNicknameRequest to NicknameRequest" #15

--- src/main/java/com/lookable/dto/user/request/NicknameRequest.java
+++ src/main/java/com/lookable/dto/user/request/NicknameRequest.java
@@ -1,14 +1,13 @@
 package com.lookable.dto.user.request;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Getter
+@Builder
 @AllArgsConstructor
-@NoArgsConstructor
-public class ChangeNicknameRequest {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Data
+public class NicknameRequest {
 
     @NotBlank(message = "닉네임을 입력해주세요.")
     private String nickname;
